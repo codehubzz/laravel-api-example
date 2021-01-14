fc92581e819560c3d8f720a2bdf10757e5ee02a9
Aleksandr Terekhov <fineworkcom@gmail.com>
2021-01-14 14:23:45 +0200

    Add: swagger

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
-use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use Illuminate\Support\Facades\Validator;
 
@@ -19,9 +18,36 @@ class AuthController extends Controller
     }
 
     /**
-     * Get a JWT via given credentials.
-     *
-     * @return \Illuminate\Http\JsonResponse
+     * @OA\Post(
+     * path="/api/login",
+     * summary="Sign in",
+     * description="Login by email, password",
+     * operationId="authLogin",
+     * tags={"auth"},
+     * @OA\RequestBody(
+     *    required=true,
+     *    description="Pass user credentials",
+     *    @OA\JsonContent(
+     *       required={"email","password"},
+     *       @OA\Property(property="email", type="string", format="email", example="user1@mail.com"),
+     *       @OA\Property(property="password", type="string", format="password", example="PassWord12345"),
+     *    ),
+     * ),
+     *   @OA\Response(
+     *     response=200,
+     *     description="Success",
+     *     @OA\JsonContent(
+     *        @OA\Property(property="user", type="object", ref="#/components/schemas/User"),
+     *     )
+     *  ),
+     * @OA\Response(
+     *    response=422,
+     *    description="Wrong credentials response",
+     *    @OA\JsonContent(
+     *       @OA\Property(property="message", type="string", example="Sorry, wrong email address or password. Please try again")
+     *        )
+     *     )
+     * )
      */
     public function login(Request $request){
         $validator = Validator::make($request->all(), [
@@ -41,9 +67,38 @@ class AuthController extends Controller
     }
 
     /**
-     * Register a User.
-     *
-     * @return \Illuminate\Http\JsonResponse
+     * @OA\Post(
+     * path="/api/register",
+     * summary="Register",
+     * description="Register by name, email, password",
+     * operationId="Register",
+     * tags={"auth"},
+     * @OA\RequestBody(
+     *    required=true,
+     *    description="Pass user credentials",
+     *    @OA\JsonContent(
+     *       required={"name","email","password"},
+     *       @OA\Property(property="name", type="string", pattern="required|string|between:2,100", example="user2"),
+     *       @OA\Property(property="email", type="string", format="email", example="user2@mail.com"),
+     *       @OA\Property(property="password", type="string", format="password", example="PassWord12345"),
+     *       @OA\Property(property="password_confirmation", type="string", format="password", example="PassWord12345"),
+     *    ),
+     * ),
+     *   @OA\Response(
+     *     response=200,
+     *     description="Success",
+     *     @OA\JsonContent(
+     *        @OA\Property(property="user", type="object", ref="#/components/schemas/User"),
+     *     )
+     *  ),
+     * @OA\Response(
+     *    response=422,
+     *    description="Wrong credentials response",
+     *    @OA\JsonContent(
+     *       @OA\Property(property="message", type="string", example="Sorry, wrong email address or password. Please try again")
+     *        )
+     *     )
+     * )
      */
     public function register(Request $request) {
         $validator = Validator::make($request->all(), [
@@ -67,11 +122,26 @@ class AuthController extends Controller
         ], 201);
     }
 
-
     /**
-     * Log the user out (Invalidate the token).
-     *
-     * @return \Illuminate\Http\JsonResponse
+     * @OA\Post(
+     * path="/api/logout",
+     * summary="Logout",
+     * description="Logout user and invalidate token",
+     * operationId="authLogout",
+     * tags={"auth"},
+     * security={ {"bearer": {} }},
+     * @OA\Response(
+     *    response=200,
+     *    description="Success"
+     *     ),
+     * @OA\Response(
+     *    response=401,
+     *    description="Returns when user is not authenticated",
+     *    @OA\JsonContent(
+     *       @OA\Property(property="message", type="string", example="Not authorized"),
+     *    )
+     * )
+     * )
      */
     public function logout() {
         auth()->logout();
@@ -89,9 +159,28 @@ class AuthController extends Controller
     }
 
     /**
-     * Get the authenticated User.
-     *
-     * @return \Illuminate\Http\JsonResponse
+     * @OA\Get(
+     * path="/api/user-profile",
+     * summary="Retrieve profile information",
+     * description="Get profile short information",
+     * operationId="profileShow",
+     * tags={"profile"},
+     * security={ {"bearer": {} }},
+     * @OA\Response(
+     *   response=200,
+     *   description="Success",
+     *   @OA\JsonContent(
+     *      @OA\Property(property="user", type="object", ref="#/components/schemas/User"),
+     *   )
+     * ),
+     * @OA\Response(
+     *    response=401,
+     *    description="User should be authorized to get profile information",
+     *    @OA\JsonContent(
+     *       @OA\Property(property="message", type="string", example="Not authorized"),
+     *    )
+     * )
+     * )
      */
     public function userProfile() {
         return response()->json(auth()->user());
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -10,6 +9,22 @@ use Illuminate\Notifications\Notifiable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
 
+/**
+ *
+ * @OA\Schema(
+ * required={"name","email","password"},
+ * @OA\Xml(name="User"),
+ * @OA\Property(property="id", type="integer", readOnly="true", example="1"),
+ * @OA\Property(property="name", type="string", pattern="required|string|between:2,100", example="John"),
+ * @OA\Property(property="email", type="string", pattern="required|string|email|max:100|unique:users", description="User unique email address", example="user@gmail.com"),
+ * @OA\Property(property="email_verified_at", type="string", readOnly="true", format="date-time", description="Datetime marker of verification status", example="2019-02-25 12:59:20"),
+ * @OA\Property(property="created_at", ref="#/components/schemas/BaseModel/properties/created_at"),
+ * @OA\Property(property="updated_at", ref="#/components/schemas/BaseModel/properties/updated_at"),
+ * )
+ *
+ * Class User
+ *
+ */
 class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable;
